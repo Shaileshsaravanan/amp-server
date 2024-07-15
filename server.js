311b6036278d815501c62cee5346fbc3d119573c
Shailesh Saravanan <77380733+Shaileshsaravanan@users.noreply.github.com>
2024-07-15 19:20:41 +0530

    added tailwind

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 const wss = new WebSocket.Server({ port: 8080 });
+let mostRecentMessage = null;
 
 wss.on('connection', function connection(ws) {
+  if (mostRecentMessage) {
+    ws.send(mostRecentMessage);
+  }
+
   ws.on('message', function incoming(message) {
     console.log('WebSocket received data: %s', message);
+    mostRecentMessage = message;  
     wss.clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN && client !== ws) {
         client.send(message);
