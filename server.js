6b96d773430e9a15dc38e99d05aafc4b85a28c2b
Shailesh Saravanan <77380733+Shaileshsaravanan@users.noreply.github.com>
2024-07-15 20:03:03 +0530

    addition of changes

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'public')));  
+app.use('/static', express.static(path.join(__dirname, 'static'))); 
 
 const wss = new WebSocket.Server({ port: 8080 });
 let mostRecentMessage = null;
